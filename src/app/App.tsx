88eb55f401df8620c8950e0972888573e642764d
xgmxgm <109265751+xgmxgm@users.noreply.github.com>
2023-06-11 19:23:30 +0600

    a
    a

--- src/app/App.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import { Layout } from './Layout/ui/Layout'
-
-export const App: FC = () => {
-  return (
-    <Layout />
-  )
-}
--- src/features/GetAllKey/index.ts
+++
@@ -1,1 +0,0 @@
-export { getAllKey } from "./GetKeys/index"
